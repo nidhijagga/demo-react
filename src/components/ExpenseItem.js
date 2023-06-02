dca139f5b871e8b878860bb528c1b86651a5a5f8
nidhijagga <nidhijagga5239@gmail.com>
2023-06-02 14:17:55 +0530

    Task 7: Adding Expenses

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,31 @@
+function ExpenseItem() {
+  return (
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col">Date</div>
+          <div class="col">Title</div>
+          <div class="col">Amount</div>
+        </div>
+        <div class="row">
+          <div class="col">2023-05-10</div>
+          <div class="col">Groceries</div>
+          <div class="col">Rs.3000</div>
+        </div>
+        <div class="row">
+          <div class="col">2023-05-10</div>
+          <div class="col">Petrol</div>
+          <div class="col">Rs.1000</div>
+        </div>
+        <div class="row">
+          <div class="col">2023-05-10</div>
+          <div class="col">Movies</div>
+          <div class="col">Rs.500</div>
+        </div>
+      </div>
+      
+    </>
+  );
+}
+
+export default ExpenseItem;
