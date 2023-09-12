d4be2983e9b55abdba2776b4972ded9a0b0b0bad
nidhijagga <nidhijagga5239@gmail.com>
2023-09-12 13:13:44 +0530

    Task 1 : Adding Event Listeners made easy

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,6 +3,10 @@ import Card from "./Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem =(props)=> {
+  const deleteExpense = () => {
+    const cardElement = document.querySelector('.expense-item');
+    cardElement.remove();
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +14,7 @@ const ExpenseItem =(props)=> {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={deleteExpense}> Delete Expense </button>
     </Card>
   );
 }
