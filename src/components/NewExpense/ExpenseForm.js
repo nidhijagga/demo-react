9f1868373f78696730a55d6908d473640211b15b
nidhijagga <nidhijagga5239@gmail.com>
2023-09-12 15:10:41 +0530

    Task 6 - Submitting the forms

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,24 +3,56 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-const [title, setTitle] = useState("");
-const [amount, setAmount] = useState("");
-const [date, setDate] = useState("");
-
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const expenseData = {
+      title: title,
+      amount: amount,
+      date: date,
+    };
+    console.log(expenseData);
+    setTitle("");
+    setAmount("");
+    setDate("");
+  };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}}/>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" value={amount} onChange={(e)=>{setAmount(e.target.value)}}/>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={amount}
+            onChange={(e) => {
+              setAmount(e.target.value);
+            }}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" value={date} onChange={(e)=>{setDate(e.target.value)}}/>
+          <input
+            type="date"
+            min="2019-01-01"
+            value={date}
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
