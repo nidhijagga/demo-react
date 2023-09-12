790c07f6874843475df926515aa4ede2af3adb17
nidhijagga <nidhijagga5239@gmail.com>
2023-09-12 14:49:26 +0530

    Task 4 - Lets introduce state inside the new Component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-const print = (e) => {
-console.log(e.target.value);
-}
+const [title, setTitle] = useState("");
+const [amount, setAmount] = useState("");
+const [date, setDate] = useState("");
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={print}/>
+          <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}}/>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={print}/>
+          <input type="number" min="0.01" step="0.01" value={amount} onChange={(e)=>{setAmount(e.target.value)}}/>
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" onChange={print} />
+          <input type="date" min="2019-01-01" value={date} onChange={(e)=>{setDate(e.target.value)}}/>
         </div>
       </div>
       <div className="new-expense__actions">
