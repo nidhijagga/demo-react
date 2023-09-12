233f24e83a9154c4db6456c3f7c6886e453a1978
nidhijaggaziprock <nidhi.jagga@ziprocktechnologies.com>
2023-09-12 12:37:05 +0530

    Task 11 - Children Props and Behind the Scenes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import ExpenseItem from './components/ExpenseItem';
-import expenseItems from './expenseItems.json';
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <div className="container">
-      <div className="row">
-          <div className="col"> <strong> Date </strong></div>
-          <div className="col"><strong> Title </strong></div>
-          <div className="col"><strong> Amount </strong></div>
-        </div>
-      </div>
-      {
-        expenseItems.expenses.map((exp)=>{
-          return <ExpenseItem date={exp.date} title={exp.title} amount={exp.amount}/>
-        })
-      }
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+import "./ExpenseItem.css";
+
 function ExpenseItem(props) {
   return (
-    <>
-      <div className="container">
-        <div className="row">
-          <div className="col">{props.date}</div>
-          <div className="col">{props.title}</div>
-          <div className="col">Rs.{props.amount}</div>
-        </div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      
-    </>
+    </Card>
   );
 }
 
